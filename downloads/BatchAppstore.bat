f4b370b52b5e498b476c5c21a89a026291624bd6
aiko1304 <132603327+aiko1304@users.noreply.github.com>
2025-06-18 15:13:31 +0200

    Update BatchAppstore.bat

--- downloads/BatchAppstore.bat
+++ downloads/BatchAppstore.bat
@@ -7,44 +7,22 @@ echo.
 echo What would you like to do?
 echo 1. Download Batch Applications
 echo 2. Exit
-echo 3. Update (Not Working)
 set /p option=Choose an option:
 
 if "%choice%"=="1" (
-    set "url=https://yourserver.com/notepad.bat"
-    set "output=apps\notepad.bat"
-    powershell -Command "Invoke-WebRequest '%url%' -OutFile '%output%'"
-    echo Done! File saved to apps\notepad.bat
-    pause
-    start "" "%output%"
-	exit
+	goto StoreMain
 )
-if "%choice%"=="2" (
-    set "url=https://yourserver.com/notepad.bat"
-    set "output=apps\notepad.bat"
-    powershell -Command "Invoke-WebRequest '%url%' -OutFile '%output%'"
-    echo Done! File saved to apps\notepad.bat
-    pause
-    start "" "%output%"
-	exit
-)
-if "%choice%"=="3" (
-    set "url=https://yourserver.com/notepad.bat"
-    set "output=apps\notepad.bat"
-    powershell -Command "Invoke-WebRequest '%url%' -OutFile '%output%'"
-    echo Done! File saved to apps\notepad.bat
-    pause
-    start "" "%output%"
+if "%option%"=="2" (
 	exit
 )
 
-:Downloads
+:StoreMain
 title Batch App Store
 cls
 echo ==== Batch App Store ====
 echo.
 echo What would you like to do?
-echo 1. Download Batch Applications
+echo 1. Install Unavailable
 echo 2. Exit
 set /p choice=Choose an option:
 
